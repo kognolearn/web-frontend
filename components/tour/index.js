708df4cf15822027cfadd23d07d3bd79b0e886a0
Decaaaa <104375071+Decaaaa@users.noreply.github.com>
2026-01-29 15:37:27 -0800

    error loading onboarding course

--- components/tour/index.js
+++ components/tour/index.js
@@ -1,3 +1,1 @@
 export { GuidedTourProvider, useGuidedTour, useTourHighlight } from './GuidedTourProvider';
-export { default as KognoTour } from './KognoTour';
-export { tourConfigs, courseCreationTour, courseFeaturesTour } from './kognoTourConfig';
