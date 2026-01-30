f8a0da4e5b15e70b22be384c1cfc12a7b8a0d3a7
Decaaaa <104375071+Decaaaa@users.noreply.github.com>
2026-01-29 19:40:09 -0800

    Revert "error loading onboarding course"
    This reverts commit 708df4cf15822027cfadd23d07d3bd79b0e886a0.

--- components/tour/index.js
+++ components/tour/index.js
@@ -1,1 +1,3 @@
 export { GuidedTourProvider, useGuidedTour, useTourHighlight } from './GuidedTourProvider';
+export { default as KognoTour } from './KognoTour';
+export { tourConfigs, courseCreationTour, courseFeaturesTour } from './kognoTourConfig';
